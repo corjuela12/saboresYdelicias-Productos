b5276702c1d8fe7c1c4dded7d711318b6114e9e4
corjuela12 <mitchellorjuela@gmail.com>
2024-05-30 10:52:59 -0500

    cambio requre de index

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php 
-require_once "vistas/parte_superior.php";
-require_once "vistas/parte_inferior.php";
+<?php require_once "vistas/parte_superior.php"?>
+<?php require_once "vistas/parte_inferior.php"?>
 
-?>
 
