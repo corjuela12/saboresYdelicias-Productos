5daffb3ebe6d510ca31e8be6b41d24cffbec039b
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2024-06-02 18:07:18 -0500

    agregue consultar_inventario.php

--- consultar_inventario.php
+++ consultar_inventario.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,19 +9,21 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Sabores & delicias</title>
+    <title>Consultar Productos</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom fonts for this template -->
     <link href="icons/font/bootstrap-icons.min.css" rel="stylesheet" type="text/css">
-    
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <!-- Custom styles for this template-->
+    <!-- Custom styles for this template -->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    
+
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 </head>
 
 <body id="page-top">
@@ -88,6 +89,7 @@
             </li>
 
             <!-- Nav Item - Inventario -->
+
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseInventario"
                     aria-expanded="true" aria-controls="collapseInventario">
@@ -97,18 +99,28 @@
                 <div id="collapseInventario" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="consultar_inventario.php">Consultar Inventario</a>
-                        <a class="collapse-item" href="#">Agregar Inventario</a>
+                        <a class="collapse-item" href="consultar_inventario.html">Consultar Inventario</a>
+                        <a class="collapse-item" href="agregar_proveedor.php">Agregar Inventario</a>
                         
                     </div>
                 </div>
             </li>
+            
 
             <!-- Nav Item - Empleados -->
             <li class="nav-item">
                 <a class="nav-link" href="#">
                     <i class="bi bi-person-fill-lock"></i>
                     <span>Empleados</span></a>
+                    <div id="collapseUtilitie" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <!--<h6 class="collapse-header">Custom Utilities:</h6>-->
+                        <a class="collapse-item" href="utilities-color.html">Crear Empleado</a>
+                        <a class="collapse-item" href="utilities-border.html">Consultar Empleado</a>
+                        <a class="collapse-item" href="utilities-animation.html">Modificar Empleado</a>
+                    </div>
+                </div>
             </li>
 
             <!-- Nav Item - venta -->
@@ -137,6 +149,14 @@
                 <a class="nav-link" href="#">
                     <i class="bi bi-calendar3"></i>
                     <span>Tienda</span></a>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <!--<h6 class="collapse-header">Custom Components:</h6>-->
+                        <a class="collapse-item" href="">Crear tienda</a>
+                        <a class="collapse-item" href="">Consultar Tienda</a>
+                        <a class="collapse-item" href="">Modificar Tienda</a>
+                    </div>
+                </div>
             </li>
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -247,5 +267,146 @@
                     </ul>
 
                 </nav>
-                <!-- End of Topbar -->
+                <!-- End of topbar -->
 
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h2 class="m-0 font-weight-bold text-primary text-center">Consultar Productos</h1>
+                    
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <!--<div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>-->
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead class="text-center">
+                                        <tr>
+                                            <th>Id producto</th>
+                                            <th>Nombre</th>
+                                            <th>Marca</th>
+                                            <th>Precio compra</th>
+                                            <th>Imagen</th>
+                                            <th>Precio venta</th>
+                                            <th>Categoria</th>
+                                            <th>Descripción</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot class="text-center">
+                                        <tr>
+                                            <th>Id producto</th>
+                                            <th>Nombre</th>
+                                            <th>Marca</th>
+                                            <th>Precio compra</th>
+                                            <th>Imagen</th>
+                                            <th>Precio Venta</th>
+                                            <th>Categoria</th>
+                                            <th>Descripción</th>
+                                            <th>Acciones</th>
+
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php
+                                        foreach($data as $dat) {
+                                        ?>
+                                        <tr class="text-center">
+                                            <td><?php echo $dat['id_producto'] ?></td>
+                                            <td><?php echo $dat['nombre'] ?></td>
+                                            <td><?php echo $dat['marca'] ?></td>
+                                            <td><?php echo $dat['precio_compra'] ?></td>
+                                            <td><img src="<?php echo $dat['img'] ?>"> </img></td>
+                                            <td><?php echo $dat['precio_venta'] ?></td>
+                                            <td><?php echo $dat['categoria'] ?></td>
+                                            <td><?php echo $dat['descripcion'] ?></td>
+                                            <td>
+                                            <div class="text-center">
+                                            <div class="btn-group">
+                                                <button class=" btn btn-primary btbEditar">Editar</button>
+                                                <button class=" btn btn-danger btnBorrar">Borrar</button>
+                                            </div>
+
+                                            </td>
+                                        </tr>
+                                        
+                                        <?php
+                                        }
+                                        ?>   
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Sabores & delicias 2024</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+
+</body>
+
+</html>
